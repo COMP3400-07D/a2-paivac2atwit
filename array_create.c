8eb50a61562a1345090b01d4ea7575c256dd3d13
paivac2atwit <paivac2@wit.edu>
2025-10-10 17:17:05 -0400

    A2

--- array_create.c
+++ array_create.c
@@ -1,9 +1,29 @@
 #include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * TODO: Describe what the function does
  */
 int* array_create_evens(int begin, int end) {
-    return NULL;
+    if (begin > end)
+        return NULL;
+
+    if (begin % 2 != 0)
+        begin++;
+
+    if (begin > end)
+        return NULL;
+
+    int length = ((end - begin) / 2) + 1;
+
+    int *arr = (int*) malloc(sizeof(int) * length);
+    if (arr = NULL)
+        return NULL;
+
+    int value = begin;
+    for(int i = 0; i < length; ++i) {
+        arr[i] = value;
+        value += 2;
+    }
+    return arr;
 }
--- string_fill.c
+++ string_fill.c
@@ -1,7 +1,18 @@
 #include <stddef.h> // For NULL
+#include <stdio.h>
+#include <iostream>
+
 
 /**
  * TODO: Describe what the function does
  */
 void string_fill(char dest[], int dest_cap, char c) {
+    if (dest == NULL || dest_cap <= 0)
+    return;
+
+    for(int i = 0; i < dest_cap - 1; i++){
+        dest[i] = c;
+    }
+
+    dest[dest_cap -1] = '\0';
 }
